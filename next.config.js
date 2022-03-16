da07f9d765d20ae1b673f38d38ba7d3feaf2bb27
mohamed amine azaiez <amine_azaiez@yahoo.fr>
2022-03-16 12:24:22 +0100

    index data5

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images:{
-      domains:["upload.wikimedia.org","a0.muscache.com","links.papareact.com","https://github.com","https://github.com/mohamed-Amine-Azaiez/framework/blob/main/data/img"]
+      domains:["upload.wikimedia.org","a0.muscache.com","links.papareact.com","https://github.com","https://github.com/mohamed-Amine-Azaiez/framework/blob/main/data/img","https://raw.githubusercontent.com/"]
   }
 }
