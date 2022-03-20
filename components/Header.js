445c2fa73b2a4c10d5ac8fc58c3b41554da29ca5
mohamed amine azaiez <amine_azaiez@yahoo.fr>
2022-03-21 04:35:05 +0800

    login

--- components/Header.js
+++ components/Header.js
@@ -10,31 +10,78 @@ import { useState } from 'react'
 import 'react-date-range/dist/styles.css' // main style file
 import 'react-date-range/dist/theme/default.css' // theme css file
 import { DateRangePicker } from 'react-date-range'
-import {useRouter} from "next/dist/client/router"
+import { useRouter } from 'next/dist/client/router'
 
-function Header({placeholder}) {
+import Login from '../components/Login'
+
+import Popup from 'reactjs-popup'
+import 'reactjs-popup/dist/index.css'
+import { useEffect } from "react";
+
+function Header({ placeholder }) {
   const [searchInput, setSearchInput] = useState('')
-  if(placeholder==="undefined"){
-    placeholder="All"
+  const [logged, setLogged] = useState("false")
+  useEffect(() => {
+    // Perform localStorage action
+    localStorage.getItem('logged')==="True"
+    ? setLogged("True")
+    : setLogged("false")
+  }, [])
+
+
+  if (placeholder === 'undefined') {
+    placeholder = 'All'
   }
-  const router = useRouter();
-
-
-  
+  const router = useRouter()
 
   const search = () => {
     router.push({
-      pathname:"/search",
-      query:{
-        location: searchInput
+      pathname: '/search',
+      query: {
+        location: searchInput,
       },
     })
+  }
+  const deconnect=() =>{
+    localStorage.setItem("logged", "False");
+        window.location.reload()
+  }
+  const checkLog = () => {
+    if (logged === "True") {
+      return (
+        <div onClick={deconnect} className="flex items-center justify-end space-x-4 text-gray-500">
+          <div className="flex items-center space-x-2 rounded-full border-2 p-2">
+            <p className="hidden cursor-pointer md:inline">Deconnexion</p>
+            <UserCircleIcon className="h-6 cursor-pointer" />
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div className="flex items-center justify-end space-x-4 text-gray-500">
+          <Popup
+            trigger={
+              <div className="flex items-center space-x-2 rounded-full border-2 p-2">
+                <p className="hidden cursor-pointer md:inline">Connexion</p>
+                <UserCircleIcon className="h-6 cursor-pointer" />
+              </div>
+            }
+            position="bottom center"
+          >
+            <Login />
+          </Popup>
+        </div>
+      )
+    }
   }
 
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white p-5 shadow-md md:px-10">
       {/* left */}
-      <div onClick={() => router.push("/")} className="relative my-auto flex h-10 cursor-pointer items-center">
+      <div
+        onClick={() => router.push('/')}
+        className="relative my-auto flex h-10 cursor-pointer items-center"
+      >
         <Image
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_Bélo.svg/2560px-Airbnb_Logo_Bélo.svg.png"
           layout="fill"
@@ -50,22 +97,16 @@ function Header({placeholder}) {
           onChange={(e) => setSearchInput(e.target.value)}
           className="flex-grow bg-transparent pl-5 text-gray-600 placeholder-gray-600 outline-none"
           type="text"
-          placeholder={placeholder || "Start your search"}
+          placeholder={placeholder || 'Start your search'}
         />
-        <SearchIcon onClick={search} className="hidden h-8 cursor-pointer rounded-full bg-red-400 p-2 text-white md:mx-2 md:inline-flex" />
+        <SearchIcon
+          onClick={search}
+          className="hidden h-8 cursor-pointer rounded-full bg-red-400 p-2 text-white md:mx-2 md:inline-flex"
+        />
       </div>
 
       {/* right */}
-      <div className="flex items-center justify-end space-x-4 text-gray-500">
-        <p className="hidden cursor-pointer md:inline">Become a host</p>
-        <GlobeAltIcon className="h-6 cursor-pointer" />
-
-        <div className="flex items-center space-x-2 rounded-full border-2 p-2">
-          <MenuIcon className="h-6 cursor-pointer" />
-          <UserCircleIcon className="h-6 cursor-pointer" />
-        </div>
-      </div>
-      
+      {checkLog()}
     </header>
   )
 }
---
+++ components/Login.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+
+function Login() {
+  const [username, setUsername] = useState('')
+  const [passw, setPassw] = useState('')
+
+
+    const loginfunc =() =>{
+      if(username==="admin"&& passw==="admin"){
+        localStorage.setItem("logged", "True");
+        window.location.reload()
+      }
+    }
+    
+  return (
+    <div className="pt-5">
+      <div className="mb-6 md:flex md:items-center">
+        <div className="">
+          <input
+            className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+            id="inline-full-name"
+            type="text"
+            placeholder="Nom d'utilisateur"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="mb-6 md:flex md:items-center">
+        <div className="">
+          <input
+          
+            className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+            id="inline-password"
+            type="password"
+            placeholder="Mot de passe"
+            onChange={(e) => setPassw(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="md:flex md:items-center content-center">
+        <button
+        onClick={loginfunc}
+          className="focus:shadow-outline rounded bg-purple-500 py-2 px-4 font-bold text-white shadow hover:bg-purple-400 focus:outline-none"
+          type="button"
+        >
+          Connexion
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Login
